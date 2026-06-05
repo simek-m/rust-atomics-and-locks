6804c14e74b83b6df8a1e4cb2f318fe239d8cdfb
Marek Simek <msimek@redhat.com>
2026-06-05 14:30:18 +0200

    Add SPSC one-shot Channel

---
+++ channel/src/lib.rs
@@ -0,0 +1,94 @@
+use std::cell::UnsafeCell;
+use std::marker::PhantomData;
+use std::mem::MaybeUninit;
+use std::sync::atomic::{AtomicBool, Ordering::*};
+use std::thread;
+use std::thread::Thread;
+
+pub struct Channel<T> {
+    msg: UnsafeCell<MaybeUninit<T>>,
+    is_ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+pub struct Sender<'a, T> {
+    channel: &'a Channel<T>,
+    receiving_thread: Thread,
+}
+
+pub struct Receiver<'a, T> {
+    channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>, // New!
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.is_ready.get_mut() {
+            unsafe { self.msg.get_mut().assume_init_drop() }
+        }
+    }
+}
+
+impl<T> Default for Channel<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            msg: UnsafeCell::new(MaybeUninit::uninit()),
+            is_ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
+        *self = Self::new();
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
+    }
+}
+
+impl<T> Sender<'_, T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.msg.get()).write(message) };
+        self.channel.is_ready.store(true, Release);
+        self.receiving_thread.unpark();
+    }
+}
+
+impl<T> Receiver<'_, T> {
+    pub fn receive(self) -> T {
+        while !self.channel.is_ready.swap(false, Acquire) {
+            thread::park();
+        }
+        unsafe { (*self.channel.msg.get()).assume_init_read() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut channel = Channel::new();
+        let (sender, receiver) = channel.split();
+        thread::scope(|s| {
+            s.spawn(|| {
+                sender.send("hello");
+            });
+            assert_eq!(receiver.receive(), "hello");
+        });
+    }
+}
